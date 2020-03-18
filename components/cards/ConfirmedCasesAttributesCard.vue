8c2a9d6052ace3487211fd001225a06bd11b20eb
pichu <pichu@tih.tw>
2020-03-18 17:47:40 +0800

    update data

--- components/cards/ConfirmedCasesAttributesCard.vue
+++ components/cards/ConfirmedCasesAttributesCard.vue
@@ -7,7 +7,9 @@
       :chart-option="{}"
       :date="Data.patients.date"
       :info="sumInfoOfPatients"
-      :url="'https://catalog.data.metro.tokyo.lg.jp/dataset/t000010d0000000068'"
+      :url="
+        'https://zh.wikipedia.org/wiki/2019%E5%86%A0%E7%8B%80%E7%97%85%E6%AF%92%E7%97%85%E8%87%BA%E7%81%A3%E7%96%AB%E6%83%85'
+      "
     />
   </v-col>
 </template>
